c92030aa9027c80827f59547c04ef3ddb19d1df9
O'Brien <obrienadedapo15@gmail.com>
2025-10-29 12:55:27 +0100

    chore: added contracts

---
+++ helper-hardhat.config.ts
@@ -0,0 +1,30 @@
+export interface networkConfigItem {
+  name?: string;
+  subscriptionId?: string;
+  gasLane?: string;
+  keepersUpdateInterval?: string;
+  raffleEntranceFee?: string;
+  callbackGasLimit?: string;
+  vrfCoordinatorV2_5?: string;
+}
+
+export interface networkConfigInfo {
+  [key: number]: networkConfigItem;
+}
+
+export const networkConfig: networkConfigInfo = {
+  31337: {
+    name: "localhost",
+  },
+  11155111: {
+    name: "sepolia",
+  },
+  1: {
+    name: "mainnet",
+  },
+};
+
+export const developmentChains = ["hardhat", "localhost"];
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
+export const frontEndContractsFile =
+  "../nextjs-smartcontract-lottery-fcc/constants/contractAddresses.json";
